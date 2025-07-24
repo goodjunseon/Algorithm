ebb8c3e7e571d2fad393f4bf697a1d809f0badf2
박준선 <168715560+goodjunseon@users.noreply.github.com>
2025-07-24 11:25:57 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        result = fibo(n);
+        System.out.println(result);
+
+    }
+
+    private static int fibo(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+}
